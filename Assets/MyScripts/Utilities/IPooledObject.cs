ad9ae8a31000cfe1a0a0638adecfc85fdb33c477
nicorm <nicorm2000@gmail.com>
2023-09-24 15:17:00 -0300

    Fixed some issues with the utilities scripts that were causing conflicts

--- Assets/MyScripts/Utilities/IPooledObject.cs
+++ Assets/MyScripts/Utilities/IPooledObject.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IPooledObject : MonoBehaviour
+namespace Utilities.Pool
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IPooledObject
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Initialize the object in question with whatever is inside this function
+        /// </summary>
+        public void OnObjectSpawn();
     }
 }
--- Assets/MyScripts/Utilities/Vector3Lerper.cs
+++ Assets/MyScripts/Utilities/Vector3Lerper.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Vector3Lerper : MonoBehaviour
+namespace Utilities.Lerpers
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Vector3Lerper : Lerper<Vector3>
     {
-        
-    }
+        /// <summary>
+        /// Call this to set vector3 lerper values, auto start if you want the timer to start or not yet
+        /// </summary>
+        public override void SetLerperValues(Vector3 start, Vector3 end, float time, LERPER_TYPE lerperType, bool autoStart = false)
+        {
+            SetValues(start, end, time, lerperType, autoStart);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /// <summary>
+        /// Call this to get the vector3 lerper actual value
+        /// </summary>
+        public override Vector3 GetValue()
+        {
+            return currentValue;
+        }
+
+        /// <summary>
+        /// Update vector3 lerper position
+        /// </summary>
+        protected override void UpdateCurrentPosition(float percentage)
+        {
+            currentValue = Vector3.Lerp(start, end, percentage);
+        }
+
+        /// <summary>
+        /// Check if the vector3 lerper has reached or not
+        /// </summary>
+        protected override bool CheckReached()
+        {
+            if (currentValue == end) return true;
+            else return false;
+        }
     }
 }
